21b1c1fecf60a2d1efecfc4d333b791ad417ea7d
diderot-sielinou <diderotsielinou@gmail.com>
2025-04-03 14:53:52 +0100

    base code

--- exercices2.js
+++ exercices2.js
@@ -1,34 +1,42 @@
-import {readFile} from 'node:fs'
+// import {readFile} from 'node:fs'
 
-const configFiles = ['config.defaults.json','config.env.json','config.local.json']
+// const configFiles = ['config.defaults.json','config.env.json','config.local.json']
 
-function loadConfig(files,index=0,configData={}){
-  if(index>= files.length){
-    console.log('final config:',configData)
-    return
-  }
-  readFile(files[index],(err,contents)=>{
-    if(err){
-      console.error('Error loading'+ files[index],err)
-    }else{
-      Object.assign(configData,JSON.parse(contents))
+// function loadConfig(files,index=0,configData={}){
+//   if(index>= files.length){
+//     console.log('final config:',configData)
+//     return
+//   }
+//   readFile(files[index],(err,contents)=>{
+//     if(err){
+//       console.error('Error loading'+ files[index],err)
+//     }else{
+//       Object.assign(configData,JSON.parse(contents))
+//     }
+//     loadConfig(files,index+1,configData)
+//   })
+// }
+// loadConfig(configFiles)
+
+import fs from 'node:fs/promises'
+
+
+async function loadConfig(files) {
+    let configData = {};
+
+    for (const file of files) {
+        try {
+            const data = await fs.readFile(file, {encoding:"utf8"});
+            console.log(` Loaded ${file}`);
+            configData = { ...configData, ...JSON.parse(data) };
+        } catch (err) {
+            console.error(` Failed to load ${file}: ${err.message}`);
+        }
     }
-    loadConfig(files,index+1,configData)
-  })
-}
-loadConfig(configFiles)
 
-function loadConfigMessage(files,index=0,configData={}){
-  if(index>= files.length){
-    console.log('final config:',configData)
-    return
-  }
-  readFile(files[index],(err,contents)=>{
-    if(err){
-      console.error('Error loading'+ files[index],err)
-    }else{
-      Object.assign(configData,JSON.parse(contents))
-    }
-    loadConfig(files,index+1,configData)
-  })
+    console.log('Final merged config:', configData);
+    return configData;
 }
+
+const configFiles = ['config.defaults.json', 'config.env.json', 'config.local.json'];
+loadConfig(configFiles);
